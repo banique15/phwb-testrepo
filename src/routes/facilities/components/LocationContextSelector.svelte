e9edd9043dce8425262ae8c84ee05765bc555b25
Javier Hinojosa <48923796+javierhinojosa@users.noreply.github.com>
2025-12-03 15:55:16 -0500

    Update 2 files [2025-12-03 20:55:15]

--- src/routes/facilities/components/LocationContextSelector.svelte
+++ src/routes/facilities/components/LocationContextSelector.svelte
@@ -5,9 +5,10 @@
 		locations: Location[]
 		selectedLocationId: number | null
 		onSelectLocation: (locationId: number | null) => void
+		onAddLocation?: () => void
 	}
 
-	let { locations, selectedLocationId, onSelectLocation }: Props = $props()
+	let { locations, selectedLocationId, onSelectLocation, onAddLocation }: Props = $props()
 </script>
 
 <div class="flex items-center gap-3 mb-4">
@@ -36,6 +37,17 @@
 		<span class="badge badge-primary badge-sm">
 			Facility View
 		</span>
+	{/if}
+	{#if onAddLocation}
+		<button
+			class="btn btn-primary btn-xs ml-auto"
+			onclick={onAddLocation}
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+			</svg>
+			Add Location
+		</button>
 	{/if}
 </div>
 
